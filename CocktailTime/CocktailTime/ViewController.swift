9d38e8511148b978c5bbf24418629113f92a23be
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-02-16 13:17:09 -0500

    added API fetch

--- CocktailTime/CocktailTime/ViewController.swift
+++ CocktailTime/CocktailTime/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 struct Course: Decodable {
     let id: Int
     let name: String
-    let imageURL: String
-    let numberOfLessons: String
+    let link: String
+    let imageUrl: String
+    let number_of_lessons: Int
 }
 
 class ViewController: UIViewController {
@@ -20,11 +21,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        fetchCoursesJson ()
+        fetchCoursesJson { (courses, error) in
+            if let error = error {
+                print("Failed to fetch courses", error)
+                return
+            }
+            
+            courses?.forEach({ (course) in
+                print(course.name)
+            })
+        }
     }
     
-    func fetchCoursesJson () {
-        print("1,2,3")
+    fileprivate func fetchCoursesJson (completion: @escaping ([Course]?, Error?) -> ()) {
+
+        let URLString = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+        
+        guard let url = URL(string: URLString) else {
+            print("Error in the url guard let")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            // Check for errors
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            // Check for good data
+            guard let data = data else {
+                print("Error in data")
+                return
+            }
+            
+            // If successful
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                completion(courses, nil)
+            } catch let jsonError {
+                completion(nil, jsonError)
+            }
+            
+            
+            
+        }.resume()
     }
 
 
